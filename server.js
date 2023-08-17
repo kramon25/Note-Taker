c89f5ba8c9102e4254048c08d19b2a362d86b3cc
unknown <ramondokenny585@gmail.com>
2023-08-17 18:08:24 -0400

    port changed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const api = require("./routes/index.js");
 const app = express();
 
 // Specify on which port the Express.js server will run
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Middleware for parsing JSON and urlendcoded form data
 app.use(express.json());
